9af669c4eeaac3cff43ea13096be6a8f554ef7b2
FouedDadi <foued.dadii@gmail.com>
2020-12-10 23:20:33 +0100

    fixing pep8

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -10,11 +10,13 @@ def canUnlockAll(boxes):
     Args:
         boxes ([type]): [description]
     """
-    seen = [0]
-    if not boxes[0] or not isinstance(boxes[0], list):
-        return False
-    for i, box in enumerate(boxes):
-        for k in box:
-            if k in range(0, len(boxes)) and k is not i and k not in seen:
-                seen.append(k)
-    return len(seen) == len(boxes)
+    seen = [False] * len(boxes)
+    seen[0] = True
+    stack = [0]
+    while stack:
+        node = stack.pop()
+        for nei in boxes[node]:
+            if not seen[nei]:
+                seen[nei] = True
+                stack.append(nei)
+    return all(seen)
