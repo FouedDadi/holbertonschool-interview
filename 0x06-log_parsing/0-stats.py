acd9d7b2a068aaae60ebe82bb21e8d65e2471b4c
FouedDadi <foued.dadii@gmail.com>
2021-01-28 20:03:12 +0100

    script that reads stdin line by line and computes metrics

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -2,22 +2,23 @@
 """Log parsing"""
 import sys
 
+
+code_dct = {"200": 0,
+            "301": 0,
+            "400": 0,
+            "401": 0,
+            "403": 0,
+            "404": 0,
+            "405": 0,
+            "500": 0}
+count = 0
+file_size = 0
+def print_status():
+    print("file size: {}".format(file_size))
+    for code in sorted(code_dct.keys()):
+        if code_dct[code] != 0:
+            print("{}: {}".format(code, code_dct[code]))
 if __name__ == "__main__":
-    code_dct = {"200": 0,
-                "301": 0,
-                "400": 0,
-                "401": 0,
-                "403": 0,
-                "404": 0,
-                "405": 0,
-                "500": 0}
-    count = 0
-    file_size = 0
-    def print_status():
-        print("file size: {}".format(file_size))
-        for code in sorted(code_dct.keys()):
-            if code_dct[code] != 0:
-                print("{}: {}".format(code, code_dct[code]))
     try:
         for line in sys.stdin:
             if len(line) > 2:
