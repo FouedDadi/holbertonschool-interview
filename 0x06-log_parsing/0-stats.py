1565f2d4fb79bb01b240449482894e96d0dd3c7a
FouedDadi <foued.dadii@gmail.com>
2021-01-28 20:33:03 +0100

    script that reads stdin line by line and computes metrics

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -1,42 +1,40 @@
 #!/usr/bin/python3
-"""script that reads stdin line by line and computes metrics
-"""
+"""Log parsing"""
 import sys
 
-code_dct = {
-        "200": 0,
-        "301": 0,
-        "400": 0,
-        "401": 0,
-        "403": 0,
-        "404": 0,
-        "405": 0,
-        "500": 0
-    }
-file_size = 0
-def print_status():
-    """[summary]
-    """
-    print("File size: {:d}".format(file_size))
-    for code in sorted(code_dct.keys()):
-        if code_dct[code]:
-            print("{}: {:d}".format(code, code_dct[code]))
 if __name__ == "__main__":
-    count = 1
+    code_dct = {"200": 0,
+                "301": 0,
+                "400": 0,
+                "401": 0,
+                "403": 0,
+                "404": 0,
+                "405": 0,
+                "500": 0}
+    count = 0
+    file_size = 0
     try:
         for line in sys.stdin:
-            if len(line) >= 3:
+            count += 1
+            if len(line) > 2:
                 words = line.split()
                 code = words[-2]
-                size = words[-1]
                 if code in code_dct.keys():
                     code_dct[code] += 1
-            file_size += int(size)
-            if count == 10:
-                print_status()
+            file_size += int(words[-1])
+            if count % 10 == 0:
+                print("File size = {}".format(file_size))
+                for code in sorted(code_dct):
+                    if code_dct[code] != 0:
+                        print("{}: {}".format(code, code_dct[code]))
                 count = 0
-            count += 1
     except KeyboardInterrupt:
-        print_status()
+        print("File size = {}".format(file_size))
+        for code in sorted(code_dct):
+            if code_dct[code] != 0:
+                print("{}: {}".format(code, code_dct[code]))
         raise
-    print_status()
+    print("File size = {}".format(file_size))
+    for code in sorted(code_dct):
+        if code_dct[code] != 0:
+            print("{}: {}".format(code, code_dct[code]))
