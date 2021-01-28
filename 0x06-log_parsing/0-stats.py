8f58d6c6cd8298a9662355939b09c1de0232769a
FouedDadi <foued.dadii@gmail.com>
2021-01-28 19:28:26 +0100

    script that reads stdin line by line and computes metrics

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    import sys
+import sys
 
+if __name__ == "__main__":
     code_dct = {"200": 0,
                 "301": 0,
                 "400": 0,
@@ -10,19 +10,30 @@ if __name__ == "__main__":
                 "404": 0,
                 "405": 0,
                 "500": 0}
-    count = 1
+    count = 0
     file_size = 0
-    for line in sys.stdin:
-        if len(line) > 2:
-            words = line.split()
-            code = words[-2]
-            if code in code_dct.keys():
-                code_dct[code] += 1
-        file_size += int(words[-1])
-        count += 1
-        if count == 10:
-            print("file size = {}".format(file_size))
-            for code in sorted(code_dct):
+    try:
+        for line in sys.stdin:
+            if len(line) > 2:
+                words = line.split()
+                code = words[-2]
+                if code in code_dct.keys():
+                    code_dct[code] += 1
+            count += 1
+            if count == 10:
+                file_size += int(words[-1])
+                print("file size = {}".format(file_size))
+                for code in sorted(code_dct):
+                    if code_dct[code] != 0:
+                        print("{}: {}".format(code, code_dct[code]))
+                count = 0
+    except KeyboardInterrupt:
+        print("file size = {}".format(file_size))
+        for code in sorted(code_dct):
+            if code_dct[code] != 0:
                 print("{}: {}".format(code, code_dct[code]))
-            count = 0
-
+        raise
+    print("file size = {}".format(file_size))
+    for code in sorted(code_dct):
+        if code_dct[code] != 0:
+            print("{}: {}".format(code, code_dct[code]))
