897bf3445ea67a301a8ef176ed2825c29cb3f476
FouedDadi <foued.dadii@gmail.com>
2021-02-04 20:52:11 +0100

    functions to check if there is a cycle in a linked list

---
+++ 0x07-linked_list_cycle/0-check_cycle.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * check_cycle - function to check if a linked list has a cycle
+ *@list: points to head of the linked list
+ * Return: return 0 if there is no cycle and 1 if there is a cycle
+ */
+int check_cycle(listint_t *list)
+{
+listint_t *p = list;
+listint_t *q = list;
+while (q && p && p->next)
+{
+p = p->next;
+q = q->next->next;
+if (q == p)
+return (1);
+}
+return (0);
+}
