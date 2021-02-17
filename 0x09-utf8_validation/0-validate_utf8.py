14df83384c6e78ecbc9b5e30248493beec501027
FouedDadi <foued.dadii@gmail.com>
2021-02-17 20:37:48 +0100

    method that determines if a set of data is utf-8 valid

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -10,49 +10,37 @@ def validUTF8(data):
     Returns:
         boolean: returns true if the set of data is UTF-8 valid or false if not
     """
-    NUMBER_OF_BITS_PER_BLOCK = 8
-    MAX_NUMBER_OF_ONES = 4
+    num_bits = 8
+    max_ones = 4
     index = 0
     while index < len(data):
         number = data[index] & (2 ** 7)
-        number >>= (NUMBER_OF_BITS_PER_BLOCK - 1)
-        if number == 0:  # single byte char
+        number >>= (num_bits - 1)
+        if number == 0:
             index += 1
             continue
-
-        # validate multi-byte char
         number_of_ones = 0
-        while True:  # get the number of significant ones
+        while True:
             number = data[index] & (2 ** (7 - number_of_ones))
-            number >>= (NUMBER_OF_BITS_PER_BLOCK - number_of_ones - 1)
+            number >>= (num_bits - number_of_ones - 1)
             if number == 1:
                 number_of_ones += 1
             else:
                 break
-
-            if number_of_ones > MAX_NUMBER_OF_ONES:
-                return False  # too much ones per char sequence
-
+            if number_of_ones > max_ones:
+                return False
         if number_of_ones == 1:
-            return False  # there has to be at least 2 ones
-
-        index += 1  # move on to check the next byte in a multi-byte char sequence
-
-        # check for out of bounds and exit early
+            return False
+        index += 1
         if index >= len(data) or index >= (index + number_of_ones - 1):
-            return False  
-
-        # every next byte has to start with "10"
+            return False
         for i in range(index, index + number_of_ones - 1):
             number = data[i]
-
-            number >>= (NUMBER_OF_BITS_PER_BLOCK - 1)
+            number >>= (num_bits - 1)
             if number != 1:
                 return False
-            number >>= (NUMBER_OF_BITS_PER_BLOCK - 1)
+            number >>= (num_bits - 1)
             if number != 0:
                 return False
-
             index += 1
-
     return True
