4d618c870513dc5a61cf7194bc733bc78412ea56
FouedDadi <foued.dadii@gmail.com>
2021-02-17 20:27:17 +0100

    method that determines if a set of data is utf-8 valid

---
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+utf-8 validator
+"""
+
+
+def validUTF8(data):
+    """method that determines if a given data set represents a valid UTF-8 encoding
+
+    Returns:
+        boolean: returns true if the set of data is UTF-8 valid or false if not
+    """
+    suc_10 = 0
+    for b in data:
+        b = bin(b).replace('0b', '').rjust(8, '0')
+        if suc_10 != 0:
+            suc_10 -= 1
+            if not b.startswith('10'):
+                return False
+        elif b[0] == '1':
+                suc_10 = len(b.split('0')[0]) - 1
+    return True
