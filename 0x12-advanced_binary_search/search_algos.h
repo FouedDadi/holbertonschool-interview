99d777079429972d470965f904d4d71de80a641b
FouedDadi <foued.dadii@gmail.com>
2021-04-29 23:55:57 +0100

    adding header file

--- 0x12-advanced_binary_search/search_algos.h
+++ 0x12-advanced_binary_search/search_algos.h
@@ -4,6 +4,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int recursivebinary(int *array, int l, int r, int value);
+void print_array(int *array, int l, int r);
 int advanced_binary(int *array, size_t size, int value);
 
 #endif
