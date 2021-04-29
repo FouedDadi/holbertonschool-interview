a5e87c59eb97aa9e955ec5bdd7cb93c3af553613
FouedDadi <foued.dadii@gmail.com>
2021-04-29 23:49:56 +0100

    adding files for advanced binary search

---
+++ 0x12-advanced_binary_search/0-advanced_binary.c
@@ -0,0 +1,60 @@
+#include "search_algos.h"
+/**
+ * print_array - function to print the array
+ * @array: the array
+ * @l: first element of array
+ * @r: last element of array
+ */
+void print_array(int *array, int l, int r)
+{
+printf("Searching in array: ");
+while (l < r)
+{
+printf("%d, ", array[l]);
+l++;
+}
+printf("%d", array[l]);
+printf("\n");
+}
+/**
+ * recursivebinary - recursive function
+ * @array: the array
+ * @l: first element of array
+ * @r: last element of array
+ * @value: value to be searched for
+ * Return: return value depending on condition
+ */
+int recursivebinary(int *array, int l, int r, int value)
+{
+if (r > l)
+{
+print_array(array, l, r);
+int mid = l + (r - l) / 2;
+if (array[mid] < value)
+return (recursivebinary(array, mid + 1, r, value));
+if (array[mid] >= value)
+return (recursivebinary(array, l, mid, value));
+if (array[mid] == value)
+return (mid);
+}
+if (array[l] == value)
+return (l);
+print_array(array, l, r);
+return (-1);
+}
+/**
+ * advanced_binary - function to search for value in array
+ * @array: array
+ * @size: size of the array
+ * @value: value to be searched for
+ * Return: return
+ */
+int advanced_binary(int *array, size_t size, int value)
+{
+size_t l = 0;
+size_t r = size - 1;
+if (!array)
+return (-1);
+return (recursivebinary(array, l, r, value));
+}
+
