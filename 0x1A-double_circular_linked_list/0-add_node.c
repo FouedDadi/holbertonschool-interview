7bf173e8d24e7ce7a483c412d2019a79aa691ab5
FouedDadi <foued.dadii@gmail.com>
2021-07-01 23:20:24 +0100

    add a new node to the genning and the end of a double circular linked list

---
+++ 0x1A-double_circular_linked_list/0-add_node.c
@@ -0,0 +1,95 @@
+#include "list.h"
+
+/**
+ * ptr_node - create a new node with str as data
+ * @str: string to be copied into the new node
+ * Return: return a pointer to ptr new node
+ */
+List *ptr_node(char *str)
+{
+struct List *ptr = NULL;
+ptr = malloc(sizeof(List));
+ptr->str = strdup(str);
+ptr->prev = NULL;
+ptr->next = NULL;
+return (ptr);
+}
+
+/**
+ * add_node_end - add a new node at the end of circular doubly linked list
+ * @list: list to be modified
+ * @str: string to be copied into the new node
+ * Return: return a pointer to ptr new node
+ */
+List *add_node_end(List **list, char *str)
+{
+struct List *temp, *ptr;
+ptr = ptr_node(str);
+if (ptr == NULL)
+{
+return (NULL);
+}
+else
+{
+ptr->str = str;
+if ((*list) == NULL)
+{
+(*list) = ptr;
+ptr->next = (*list);
+ptr->prev = (*list);
+}
+else
+{
+temp = (*list);
+while (temp->next != (*list))
+{
+temp = temp->next;
+}
+temp->next = ptr;
+ptr->prev = temp;
+(*list)->prev = ptr;
+ptr->next = (*list);
+}
+}
+return (ptr);
+}
+
+/**
+ * add_node_begin - add a new node at the beginning of circular D-L-L
+ * @list: list to be modified
+ * @str: string to be copied into the new node
+ * Return: return a pointer to ptr new node
+ */
+List *add_node_begin(List **list, char *str)
+{
+struct List *temp, *ptr;
+ptr = ptr_node(str);
+if (ptr == NULL)
+{
+return (NULL);
+}
+else
+{
+ptr->str = str;
+if ((*list) == NULL)
+{
+(*list) = ptr;
+ptr->next = (*list);
+ptr->prev = (*list);
+}
+else
+{
+temp = (*list);
+while (temp->next != (*list))
+{
+temp = temp->next;
+}
+temp->next = ptr;
+ptr->prev = temp;
+(*list)->prev = ptr;
+ptr->next = (*list);
+*list = ptr;
+}
+}
+return (ptr);
+}
