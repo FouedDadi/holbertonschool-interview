31e93bccd5bb61490e35826470a8148436276eb9
FouedDadi <foued.dadii@gmail.com>
2021-07-08 14:26:28 +0100

    function that sorts an array of integers in ascending order using the LSD Radix sort algorithm

--- 0x1B-radix_sort/0-radix_sort.c
+++ 0x1B-radix_sort/0-radix_sort.c
@@ -1,49 +1,56 @@
 #include "sort.h"
 /**
+ * getMax - get the maximum digit
+ * @array: array
+ * @size: size of array
+ * Return: return the maximum digit
+ */
+int getMax(int array[], size_t size)
+{
+int n = size;
+int mx = array[0];
+int i;
+for (i = 1; i < n; i++)
+if (array[i] > mx)
+mx = array[i];
+return (mx);
+}
+/**
+ * countSort - count sort
+ * @array: array
+ * @size: size of array
+ * @exp: exp
+ */
+void countSort(int *array, size_t size, int exp)
+{
+int n = size;
+int output[n];
+int i, count[10] = { 0 };
+for (i = 0; i < n; i++)
+count[(array[i] / exp) % 10]++;
+for (i = 1; i < 10; i++)
+count[i] += count[i - 1];
+for (i = n - 1; i >= 0; i--)
+{
+output[count[(array[i] / exp) % 10] - 1] = array[i];
+count[(array[i] / exp) % 10]--;
+}
+for (i = 0; i < n; i++)
+array[i] = output[i];
+}
+/**
  * radix_sort - LSD radix sort algorithm
- * @array: Array
- * @size: Size of the array
+ * @array: array
+ * @size: size of array
  */
 void radix_sort(int *array, size_t size)
 {
-int j;
-int i;
-int siz = size;
-int maximum = array[0];
-for (i = 1; i < siz; i++)
+int n = size;
+int m = getMax(array, n);
+int exp;
+for (exp = 1; m / exp > 0; exp *= 10)
 {
-if (maximum < array[i])
-maximum = array[i];
-}
-int digits = 0;
-while (maximum > 0)
-{
-digits++;
-maximum /= 10;
-}
-int power = 1;
-for (i = 0; i < digits; i++)
-{
-int new_array[siz];
-int count[10];
-for (j = 0; j < 10; j++)
-count[j] = 0;
-for (j = 0; j < siz; j++)
-{
-int num = (array[j] / power) % 10;
-count[num]++;
-}
-for (j = 1 ; j < 10; j++)
-count[j] += count[j - 1];
-for (j = siz - 1; j >= 0; j--)
-{
-int num = (array[j] / power) % 10;
-new_array[count[num] - 1] = array[j];
-count[num]--;
-}
-for (j = 0 ; j < siz; j++)
-array[j] = new_array[j];
-power *= 10;
+countSort(array, n, exp);
 print_array(array, size);
 }
 }
