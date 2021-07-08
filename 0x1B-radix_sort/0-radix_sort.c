d501ad69e205719ca2ee6e92a00de3d46aaed581
FouedDadi <foued.dadii@gmail.com>
2021-07-08 14:52:09 +0100

    function that sorts an array of integers in ascending order using the LSD Radix sort algorithm

--- 0x1B-radix_sort/0-radix_sort.c
+++ 0x1B-radix_sort/0-radix_sort.c
@@ -1,60 +1,57 @@
 #include "sort.h"
-
 /**
- * counting_sort_radix - Counting sort
- * @array: Array to be sorted
- * @size: Size of the @array
- * @exp: Exponent
- * Return: Nothing.
+ * getMax - get the maximum digit
+ * @array: array
+ * @size: size of array
+ * Return: return the maximum digit
  */
-void counting_sort_radix(int *array, size_t size, int exp)
+int getMax(int *array, size_t size)
 {
-	int freq[10] = {0}, j;
-	int *output = NULL;
-	size_t i;
-
-	output = malloc(sizeof(int) * size);
-	if (!output)
-		return;
-	for (i = 0; i < size; i++)
-		freq[(array[i] / exp) % 10]++;
-	for (i = 1; i < 10; i++)
-		freq[i] += freq[i - 1];
-	for (j = size - 1; j >= 0; j--)
-	{
-		output[freq[(array[j] / exp) % 10] - 1] = array[j];
-		freq[(array[j] / exp) % 10]--;
-	}
-	for (i = 0; i < size; i++)
-		array[i] = output[i];
-	print_array(array, size);
-	free(output);
+int mx = array[0];
+uint i;
+for (i = 1; i < size; i++)
+if (array[i] > mx)
+mx = array[i];
+return (mx);
 }
-
 /**
- * radix_sort - Radix sorting
- * @array: Array to be sorted
- * @size: Size of the @array
- * Return: Nothing.
+ * countSort - count sort
+ * @array: array
+ * @size: size of array
+ * @exp: exp
+ */
+void countSort(int *array, size_t size, int exp)
+{
+int *output;
+output = malloc(sizeof(int) * size);
+size_t i;
+int x;
+int count[10] = { 0 };
+for (i = 0; i < size; i++)
+count[(array[i] / exp) % 10]++;
+for (i = 1; i < 10; i++)
+count[i] += count[i - 1];
+for (x = size - 1; x >= 0; x--)
+{
+output[count[(array[x] / exp) % 10] - 1] = array[x];
+count[(array[x] / exp) % 10]--;
+}
+for (i = 0; i < size; i++)
+array[i] = output[i];
+free(output);
+}
+/**
+ * radix_sort - LSD radix sort algorithm
+ * @array: array
+ * @size: size of array
  */
 void radix_sort(int *array, size_t size)
 {
-	uint i = 1;
-	int max = 0, exp = 1;
-
-	if (!array || size < 2)
-		return;
-	max = array[0];
-	while (i < size)
-	{
-		if (array[i] > max)
-			max = array[i];
-		i++;
-	}
-	while (max)
-	{
-		counting_sort_radix(array, size, exp);
-		exp *= 10;
-		max /= 10;
-	}
+int m = getMax(array, size);
+int exp;
+for (exp = 1; m / exp > 0; exp *= 10)
+{
+countSort(array, size, exp);
+print_array(array, size);
+}
 }
