211cc2ef6c4f37755148b95db01aa6e1fce2d8a3
FouedDadi <foued.dadii@gmail.com>
2021-07-14 15:02:46 +0100

    function that returns the perimeter of the island described in grid

---
+++ 0x1C-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""island perimiter problem"""
+
+
+def island_perimeter(grid):
+    """function that returns the perimeter of the island described in grid
+
+    Args:
+        grid ([type]): [description]
+
+    Returns:
+        [type]: [description]
+    """
+    count = 0
+    m = len(grid)
+    if m == 0:
+        return 0
+    n = len(grid[0])
+    for i in range(m):
+        for j in range(n):
+            if grid[i][j] == 1:
+                count += 4
+                if i - 1 >= 0 and grid[i-1][j] == 1:
+                    count -= 2
+                if j - 1 >= 0 and grid[i][j-1] == 1:
+                    count -= 2
+    return count
