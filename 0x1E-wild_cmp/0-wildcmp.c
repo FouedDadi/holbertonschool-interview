26611a49151f0ac5a4d62e8e6af08f2dce571a15
FouedDadi <foued.dadii@gmail.com>
2021-07-29 19:50:45 +0100

    function that compare two strings with one containing wildcard '*'

--- 0x1E-wild_cmp/0-wildcmp.c
+++ 0x1E-wild_cmp/0-wildcmp.c
@@ -15,7 +15,7 @@ if (*s1 == *s2)
 return (wildcmp(s1 + 1, s2 + 1));
 if (*s2 == '*')
 {
-if (*s1 == '\0' && *s2 != '\0')
+if (*s1 == '\0')
 return (wildcmp(s1, s2 + 1));
 else
 return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
