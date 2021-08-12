072b2fa3f7c6e2815d503f477035521cdeb9614b
FouedDadi <foued.dadii@gmail.com>
2021-08-12 23:45:09 +0100

    adding function that checks whether a given pattern matches a given string.

--- 0x20-regex/regex.c
+++ 0x20-regex/regex.c
@@ -8,18 +8,14 @@
  */
 int regex_match(char const *str, char const *pattern)
 {
-if (!str || !pattern)
-return (0);
 if (*str == '\0' && *pattern == '\0')
 return (1);
 if (*pattern == '.' || *str == *pattern)
 return (regex_match(str + 1, pattern + 1));
 if (*pattern == '*')
 {
-if (*str == '\0')
-return (regex_match(str, pattern + 1));
-else
-return (regex_match(str + 1, pattern) || regex_match(str, pattern + 1));
+if (*str != '\0' && (*str == *pattern || *pattern == '.'))
+return ((regex_match(str, pattern + 2) || regex_match(str + 1, pattern)));
 }
 return (0);
 }
