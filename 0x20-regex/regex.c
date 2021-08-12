7d6aa3029167c0d8b5d28b82507114348c3ead73
FouedDadi <foued.dadii@gmail.com>
2021-08-12 23:36:08 +0100

    adding function that checks whether a given pattern matches a given string.

---
+++ 0x20-regex/regex.c
@@ -0,0 +1,23 @@
+#include "regex.h"
+
+/**
+ * regex_match - compare string and pattern
+ * @str: string
+ * @pattern: string with special char . or/and *
+ * Return: return 1 or 0
+ */
+int regex_match(char const *str, char const *pattern)
+{
+if (*str == '\0' && *pattern == '\0')
+return (1);
+if (*pattern == '.' || *str == *pattern)
+return (regex_match(str + 1, pattern + 1));
+if (*pattern == '*')
+{
+if (*str == '\0')
+return (regex_match(str, pattern + 1));
+else
+return (regex_match(str + 1, pattern) || regex_match(str, pattern + 1));
+}
+return (0);
+}
